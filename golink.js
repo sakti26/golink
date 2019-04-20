86e5a88a527cb9471ab0d1f51271aafef3b92a90
sakti26 <42515518+sakti26@users.noreply.github.com>
2019-04-20 12:10:07 +0700

    Add files via upload

--- golink.js
+++ golink.js
@@ -131,7 +131,7 @@ function auto_safeconvert() {
         }
         if (a_to_vh == false) {
             var encryptedUrl = Base64.encode(a_to_vi);
-            a_to_ve[i].href = "https://www.ayungeblog.com/mark-drakeford-tory-arrogance-could-damage-uk/?url=" + encryptedUrl;
+            a_to_ve[i].href = "https://www.ayungeblog.com/tos/?url=" + encryptedUrl;
             a_to_ve[i].rel = "nofollow";
             a_to_vb++;
             a_to_vc += i + ":::" + a_to_ve[i].href + "\n";
